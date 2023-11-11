599dcbf84bfe6041d3b0a7ba1bc8414f86bb38b4
spring-raining <harusamex.com@gmail.com>
2023-11-11 14:37:36 +0900

    Improve the www template

--- www/src/consts.ts
+++ www/src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = 'Astro & Vivliostyleサンプルページ';
+export const SITE_TITLE = 'Astro & Vivliostyleテンプレートページ';
 export const SITE_DESCRIPTION =
-  'AstroとVivliostyleを使ってWebと出版物の両方を同時に執筆します。';
+  'AstroとVivliostyleを使ってWebと出版物の両方を同時に執筆するためのテンプレートページです。';
